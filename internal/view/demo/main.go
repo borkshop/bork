52be293b87c0e7a732dca75f1c28532784eee254
Joshua T Corbin <jcorbin@wunjo.org>
2017-12-06 17:53:11 -0800

    Use perf and its dash in hud demo

--- internal/view/demo/main.go
+++ internal/view/demo/main.go
@@ -4,16 +4,29 @@ import (
 	"log"
 	"math/rand"
 
+	"github.com/borkshop/bork/internal/perf"
 	"github.com/borkshop/bork/internal/point"
 	"github.com/borkshop/bork/internal/view"
 	"github.com/borkshop/bork/internal/view/hud"
 )
 
-type world struct {
+type ui struct {
 	hud.Logs
+	perf.Dash
+}
+
+type world struct {
+	perf perf.Perf
+	ui
 
 	table []rune
 	grid  view.Grid
+}
+
+func (w *world) init() {
+	w.Logs.Init(1000)
+	w.perf.Init("demo", nil) // TODO pass your root ecs.Proc in here
+	w.ui.Perf = &w.perf
 }
 
 func (w *world) generate(sz point.Point) {
@@ -29,6 +42,8 @@ func (w *world) Render(termGrid view.Grid) error {
 		World: w.grid, // TODO render your world grid and pass it here
 	}
 
+	hud.AddRenderable(&w.ui.Dash, view.AlignRight|view.AlignBottom|view.AlignHFlush)
+
 	// TODO call hud methods to build a basic UI, e.g.:
 	hud.HeaderF("<left1")
 	hud.HeaderF("<left2")
@@ -37,12 +52,7 @@ func (w *world) Render(termGrid view.Grid) error {
 	hud.HeaderF("center by default")
 
 	hud.FooterF("use h/j/k/l and y/u/b/n to change grid size")
-	hud.FooterF(">one")
-	hud.FooterF(">two")
-	hud.FooterF(".>three") // the "." forces a new line
-
-	// NOTE more advanced UI components may use:
-	// hud.AddRenderable(ren view.Renderable, align view.Align)
+	hud.FooterF(".>right footer") // the "." forces a new line
 
 	hud.Render(termGrid)
 	return nil
@@ -79,7 +89,7 @@ func (w *world) HandleKey(k view.KeyEvent) error {
 func main() {
 	if err := view.JustKeepRunning(func(v *view.View) (view.Client, error) {
 		var w world
-		w.Logs.Init(1000)
+		w.init()
 
 		w.table = []rune{
 			'_', '-',
