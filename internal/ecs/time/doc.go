1b97f7a1a64d9d7b333e0d1b3d47e89d7a499bbc
Joshua T Corbin <jcorbin@wunjo.org>
2017-12-07 18:30:46 -0800

    Add an example test to time

--- internal/ecs/time/doc.go
+++ internal/ecs/time/doc.go
@@ -1,4 +1,4 @@
-/*Package ecsTime provides a Facility for managing time in an ecs.System.
+/*Package time provides a Facility for managing time in an ecs.System.
 
 Time in an ecs.System is counted as the number of Process()ing rounds that have
 happened (counting the current one!); so Time(0) will never be seen in
@@ -10,4 +10,4 @@ time. Such a setup might make sense if there's a non-trivial relationship
 between toplevel Process()ing and an inner system's Process()ing.
 
 */
-package ecsTime
+package time
