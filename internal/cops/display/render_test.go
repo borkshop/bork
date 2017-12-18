80e26fbb0be18280e3a4ce53f3a6a8c1eaa8a58a
Joshua T Corbin <jcorbin@wunjo.org>
2017-12-17 20:47:42 -0800

    cops: generalize render integration test

--- internal/cops/display/render_test.go
+++ internal/cops/display/render_test.go
@@ -1,6 +1,7 @@
 package display_test
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"strings"
@@ -46,27 +47,64 @@ func TestRender_blankAndMultiRuneCellOver(t *testing.T) {
 	assert.Equal(t, []byte(" "+whiteHand+"\r\033[2C "), buf)
 }
 
-func TestRender_model8(t *testing.T) {
-	dis := New(image.Rect(0, 0, 16, 8))
-	for y := 0; y < 8; y++ {
-		b := Colors[y]
-		for x := 0; x < 16; x++ {
-			f := Colors[x/2]
-			dis.SetRGBA(x, y, "<", f, b)
-			x++
-			dis.SetRGBA(x, y, ">", f, b)
-		}
+func TestRender_integration(t *testing.T) {
+	for _, tc := range []struct {
+		name     string
+		size     image.Point
+		model    ColorModel
+		setup    func(x, y int) (t string, f, b color.RGBA)
+		expected []string
+	}{
+		{
+			name:  "model3 coverage",
+			size:  image.Pt(16, 8),
+			model: Model3,
+			setup: func(x, y int) (t string, f, b color.RGBA) {
+				if x%2 == 0 {
+					t = "<"
+				} else {
+					t = ">"
+				}
+				return t, Colors[(x/2)%8], Colors[y%8]
+			},
+			expected: []string{
+				"[30m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
+				"[30m[41m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
+				"[30m[42m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
+				"[30m[43m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
+				"[30m[44m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
+				"[30m[45m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
+				"[30m[46m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
+				"[30m[47m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>[m",
+			},
+		},
+	} {
+		// TODO also cur = Start
+
+		t.Run(fmt.Sprintf("%s Set", tc.name), func(t *testing.T) {
+			dis := New(image.Rect(0, 0, tc.size.X, tc.size.Y))
+			for y := 0; y < 8; y++ {
+				for x := 0; x < 16; x++ {
+					t, f, b := tc.setup(x, y)
+					dis.Set(x, y, t, f, b)
+				}
+			}
+			buf, _ := Render(nil, Reset, dis, tc.model)
+			assert.Equal(t, tc.expected, strings.Split(string(buf), "\r\n"))
+		})
+
+		t.Run(fmt.Sprintf("%s SetRGBA", tc.name), func(t *testing.T) {
+			dis := New(image.Rect(0, 0, tc.size.X, tc.size.Y))
+			for y := 0; y < 8; y++ {
+				for x := 0; x < 16; x++ {
+					t, f, b := tc.setup(x, y)
+					dis.SetRGBA(x, y, t, f, b)
+				}
+			}
+			buf, _ := Render(nil, Reset, dis, tc.model)
+			assert.Equal(t, tc.expected, strings.Split(string(buf), "\r\n"))
+		})
+
 	}
-	// TODO also cur = Start
-	buf, _ := Render(nil, Reset, dis, Model3)
-	assert.Equal(t, []string{
-		"[30m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
-		"[30m[41m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
-		"[30m[42m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
-		"[30m[43m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
-		"[30m[44m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
-		"[30m[45m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
-		"[30m[46m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>",
-		"[30m[47m<>[31m<>[32m<>[33m<>[34m<>[35m<>[36m<>[37m<>[m",
-	}, strings.Split(string(buf), "\r\n"))
+
 }
