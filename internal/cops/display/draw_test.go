fb21c6d51c6e7bcb1822d99a6bb4c5e8b4946543
Joshua T Corbin <jcorbin@wunjo.org>
2017-12-18 19:41:50 -0800

    cops: generalize draw test

--- internal/cops/display/draw_test.go
+++ internal/cops/display/draw_test.go
@@ -12,62 +12,58 @@ import (
 )
 
 func TestDraw_centered(t *testing.T) {
-	dst := New(image.Rect(0, 0, 16, 8))
-	src := New(image.Rect(0, 0, 8, 4))
-	dst.Fill(dst.Rect, "_", Colors[7], Colors[0])
-	src.Fill(src.Rect, "x", Colors[5], Colors[4])
+	for _, tc := range []struct {
+		name     string
+		dstSize  image.Point
+		srcSize  image.Point
+		expected []string
+	}{
+		{
+			name:    "src < dst",
+			dstSize: image.Pt(16, 8),
+			srcSize: image.Pt(8, 4),
+			expected: []string{
+				"________________",
+				"________________",
+				"____xxxxxxxx____",
+				"____xxxxxxxx____",
+				"____xxxxxxxx____",
+				"____xxxxxxxx____",
+				"________________",
+				"________________",
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			dst := New(image.Rectangle{Max: tc.dstSize})
+			src := New(image.Rectangle{Max: tc.srcSize})
+			dst.Fill(dst.Rect, "_", Colors[7], Colors[0])
+			src.Fill(src.Rect, "x", Colors[5], Colors[4])
 
-	bound, off := dst.Rect, image.ZP
-	if n := bound.Dx() - src.Rect.Dx(); n > 0 {
-		bound.Min.X += n / 2
-	} else if n < 0 {
-		off.X += n / 2
+			bound, off := dst.Rect, image.ZP
+			if n := bound.Dx() - src.Rect.Dx(); n > 0 {
+				bound.Min.X += n / 2
+			} else if n < 0 {
+				off.X += n / 2
+			}
+			if n := bound.Dy() - src.Rect.Dy(); n > 0 {
+				bound.Min.Y += n / 2
+			} else if n < 0 {
+				off.Y += n / 2
+			}
+			Draw(dst, bound, src, off, draw.Over)
+
+			assert.Equal(t, tc.expected, dst.Text.LinesWithFill("0"), "expected textile draw")
+			assert.Equal(t, tc.expected, imageTestRepr(dst.Foreground, "0", map[color.RGBA]string{
+				Colors[7]: "_",
+				Colors[5]: "x",
+			}), "expected foreground draw")
+			assert.Equal(t, tc.expected, imageTestRepr(dst.Background, "0", map[color.RGBA]string{
+				Colors[0]: "_",
+				Colors[4]: "x",
+			}), "expected background draw")
+		})
 	}
-	if n := bound.Dy() - src.Rect.Dy(); n > 0 {
-		bound.Min.Y += n / 2
-	} else if n < 0 {
-		off.Y += n / 2
-	}
-	Draw(dst, bound, src, off, draw.Over)
-
-	assert.Equal(t, []string{
-		"________________",
-		"________________",
-		"____xxxxxxxx____",
-		"____xxxxxxxx____",
-		"____xxxxxxxx____",
-		"____xxxxxxxx____",
-		"________________",
-		"________________",
-	}, dst.Text.LinesWithFill("0"))
-
-	assert.Equal(t, []string{
-		"................",
-		"................",
-		"....!!!!!!!!....",
-		"....!!!!!!!!....",
-		"....!!!!!!!!....",
-		"....!!!!!!!!....",
-		"................",
-		"................",
-	}, imageTestRepr(dst.Foreground, "0", map[color.RGBA]string{
-		Colors[7]: ".",
-		Colors[5]: "!",
-	}))
-
-	assert.Equal(t, []string{
-		"----------------",
-		"----------------",
-		"----########----",
-		"----########----",
-		"----########----",
-		"----########----",
-		"----------------",
-		"----------------",
-	}, imageTestRepr(dst.Background, "0", map[color.RGBA]string{
-		Colors[0]: "-",
-		Colors[4]: "#",
-	}))
 }
 
 func imageTestRepr(img *image.RGBA, dflt string, c2s map[color.RGBA]string) (r []string) {
